3112a924f3d95841a758d3db5cde63d8dc0ea064
Nikint86 <lobackiy_2007@mail.ru>
2024-12-22 14:48:26 +0300

    Убрал зависимости

--- time_img_send.py
+++ time_img_send.py
@@ -3,22 +3,26 @@ import telegram
 import os
 from dotenv import load_dotenv
 from time import sleep
+import argparse
 
-
-DOCUMENT = os.listdir(os.path.join("C:","Python","image"))
-
-
-def main():
+def main(image_directory):
     load_dotenv()
     token = os.getenv("TG_BOT_TOKEN")
     chat_id = os.getenv("GROUP_TG_CHAT_ID")
     time = int(os.getenv("BOT_TIMER"))
+    DOCUMENT = os.listdir(image_directory)
+
     while True:
         random.shuffle(DOCUMENT)
         for rand_choice in DOCUMENT:
             bot = telegram.Bot(token=token)
-            with open(os.path.join("C:", "Python", "image" f"{rand_choice}", "rb")) as document_file:
+            document_path = os.path.join(image_directory, rand_choice)
+            with open(document_path, "rb") as document_file:
                 bot.send_document(chat_id=chat_id, document=document_file)
             sleep(time)
 
-main()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Send random images to Telegram group")
+    parser.add_argument('directory', type=str, help='Directory containing images')
+    args = parser.parse_args()
+    main(args.directory)
