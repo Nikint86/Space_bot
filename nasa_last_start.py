205537558ccf57a01ec82ad46a154fe716257e78
Nikint86 <lobackiy_2007@mail.ru>
2024-12-25 20:21:40 +0300

    Поменял структуру кода в if __name__ == '__main__':

--- nasa_last_start.py
+++ nasa_last_start.py
@@ -14,7 +14,13 @@ def download_image(image_url, image_number, image_directory):
     except requests.exceptions.RequestException as e:
         print(f"Ошибка при загрузке изображения {image_url}: {e}")
 
-def main(image_directory):
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Скачать фото с последнего запуска NASA.")
+    parser.add_argument('directory', type=str, help='Директория для сохранения фото.')
+    args = parser.parse_args()
+    image_directory = Path(args.directory)
+    image_directory.mkdir(parents=True, exist_ok=True)
     spacex_url = "https://api.spacexdata.com/v5/launches/5eb87d42ffd86e000604b384"
     response = requests.get(spacex_url)
     response.raise_for_status()
@@ -24,13 +30,4 @@ def main(image_directory):
     for original_number, original in enumerate(spacex_links, start=1):
         print(f"Загружается изображение {original_number}: {original}")
         download_image(original, original_number, image_directory)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Скачать фото с последнего запуска NASA.")
-    parser.add_argument('directory', type=str, help='Директория для сохранения фото.')
-    args = parser.parse_args()
-    image_directory = Path(args.directory)
-    image_directory.mkdir(parents=True, exist_ok=True)
-    main(image_directory)
-
 
--- time_img_send.py
+++ time_img_send.py
@@ -5,24 +5,25 @@ from dotenv import load_dotenv
 from time import sleep
 import argparse
 
-def main(image_directory):
+def main():
     load_dotenv()
     token = os.environ["TG_BOT_TOKEN"]
     chat_id = os.environ["GROUP_TG_CHAT_ID"]
     time = int(os.environ["BOT_TIMER"])
-    open_directory = os.listdir(image_directory)
-
+    parser = argparse.ArgumentParser(description="Отправить случайную картинку в группу Telegram")
+    parser.add_argument('directory', type=str, help='Директория с картинками.')
+    args = parser.parse_args()
+    open_directory = os.listdir(args.directory)
+    bot = telegram.Bot(token=token)
     while True:
         random.shuffle(open_directory)
         for rand_choice in open_directory:
-            bot = telegram.Bot(token=token)
-            document_path = os.path.join(image_directory, rand_choice)
+            document_path = os.path.join(args.directory, rand_choice)
             with open(document_path, "rb") as document_file:
                 bot.send_document(chat_id=chat_id, document=document_file)
             sleep(time)
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Send random images to Telegram group")
-    parser.add_argument('directory', type=str, help='Directory containing images')
-    args = parser.parse_args()
-    main(args.directory)
+    main()
+
