c887b5e80386dd861622c8ddd09b3069cf31d0cd
Nikint86 <lobackiy_2007@mail.ru>
2025-01-08 20:13:33 +0300

    Убрал дублирование

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,15 +1,7 @@
 import requests
 from pathlib import Path
 import argparse
-
-def download_image(image_url, image_number, image_directory):
-        response = requests.get(image_url, stream=True)
-        response.raise_for_status()
-        filename = image_directory / f"space_x_{image_number}.jpg"
-        with open(filename, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                file.write(chunk)
-        print(f"Изображение {image_number} успешно загружено: {filename}")
+from save_tool import download_and_save_image
 
 
 if __name__ == '__main__':
@@ -30,5 +22,5 @@ if __name__ == '__main__':
 
     for original_number, original in enumerate(spacex_links, start=1):
         print(f"Загружается изображение {original_number}: {original}")
-        download_image(original, original_number, image_directory)
+        download_and_save_image(original, original_number, image_directory)
 
