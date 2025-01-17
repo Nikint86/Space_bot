b6554ea7115ff331644d64a279613c0f3a482698
Nikint86 <lobackiy_2007@mail.ru>
2025-01-18 02:00:28 +0300

    Переименовал переменную

--- save_tool.py
+++ save_tool.py
@@ -4,7 +4,7 @@ import requests
 def download_and_save_image(image_url, index, directory):
     image_response = requests.get(image_url, stream=True)
     image_response.raise_for_status()
-    filename = directory / f"saved_image{index + 1}.jpg"
-    with open(filename, 'wb') as file:
+    filepath = directory / f"saved_image{index + 1}.jpg"
+    with open(filepath, 'wb') as file:
        file.write(image_response.content)
-    print(f"Изображение {index + 1} успешно загружено: {filename}")
+    print(f"Изображение {index + 1} успешно загружено: {filepath}")
