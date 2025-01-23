73c1984a41fc979f51a2f593a558524d08cbe751
Nikint86 <lobackiy_2007@mail.ru>
2025-01-23 16:07:39 +0300

    Исправил переменную, убрал перерасчёт индексов

--- download_nasa_apod.py
+++ download_nasa_apod.py
@@ -20,18 +20,18 @@ def main():
 
     load_dotenv()
     api_key = os.environ["NASA_API_KEY"]
-    num_images = 30
-    payload = {"api_key": api_key, "count": f"{num_images}"}
-    nasa_urls = "https://api.nasa.gov/planetary/apod"
+    images_num= 30
+    payload = {"api_key": api_key, "count": f"{images_num}"}
+    nasa_url = "https://api.nasa.gov/planetary/apod"
 
-    nasa_response = requests.get(nasa_urls, params=payload)
+    nasa_response = requests.get(nasa_url, params=payload)
     nasa_response.raise_for_status()
     images_data = nasa_response.json()
 
     for index, item in enumerate(images_data, start=1):
         image_nasa_url = item.get("url")
         if image_nasa_url:
-            print(f"Загружается изображение {index + 1}: {image_nasa_url}")
+            print(f"Загружается изображение: {image_nasa_url}")
             download_and_save_image(image_nasa_url, index, image_directory)
 
 
