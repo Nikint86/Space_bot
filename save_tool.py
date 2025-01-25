7a4b7984e6f5c678de82e187520188dd43cc1069
Nikint86 <lobackiy_2007@mail.ru>
2025-01-25 18:57:36 +0300

    сделал название фото уникальными

--- save_tool.py
+++ save_tool.py
@@ -4,7 +4,7 @@ import requests
 def download_and_save_image(image_url, directory):
     image_response = requests.get(image_url, stream=True)
     image_response.raise_for_status()
-    filepath = directory / f"saved_image.jpg"
+    filepath = directory / f"{image_url}.jpg"
     with open(filepath, 'wb') as file:
        file.write(image_response.content)
     print(f"Изображение успешно загружено: {filepath}")
